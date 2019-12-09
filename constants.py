c4d223b28dd3ac4c727e4cecc4d4614f0b16abef
Bukagg <nirolgg@gmail.com>
2019-12-09 11:14:20 +0200

    bug fixes + added preprocess functions for user inputs

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+from chatterbot.response_selection import get_most_frequent_response
+
 CHAT_BOT_NAME = "Nir ChatBot"
 DATABASE_NAME = "chatbotdb"
 DB_URL = 'sqlite:///' + DATABASE_NAME +'.sqlite3'
@@ -6,4 +8,25 @@ DB_URL = 'sqlite:///' + DATABASE_NAME +'.sqlite3'
 STORAGE_ADAPTER = 'chatterbot.storage.SQLStorageAdapter'
 
 
-LOGIC_ADAPTERS = ['chatterbot.logic.MathematicalEvaluation','chatterbot.logic.TimeLogicAdapter']
+LOGIC_ADAPTERS = ['chatterbot.logic.MathematicalEvaluation',
+                  {
+                      'import_path': 'chatterbot.logic.BestMatch',
+                      'default_response': 'I am sorry, but I do not understand.',
+                      'maximum_similarity_threshold': 0.90
+                  },
+                  {
+                      'import_path': 'chatterbot.logic.SpecificResponseAdapter',
+                      'input_text': 'What is your name?',
+                      'output_text': 'My Name is Nir Chat Bot!'
+                  }
+                  ]
+
+PREPROCESSORS=[
+        'chatterbot.preprocessors.clean_whitespace',
+            'chatterbot.preprocessors.unescape_html',
+            'chatterbot.preprocessors.convert_to_ascii'
+    ]
+
+
+
+RESPONSE_SELECTION_METHOD = get_most_frequent_response
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from pip._vendor.distlib.compat import raw_input
+
 import constants
 import init_train
-
+from chatterbot.response_selection import get_most_frequent_response
 
 
 
@@ -12,7 +13,9 @@ def init_chat_bot():
         constants.CHAT_BOT_NAME,
         storage_adapter= constants.STORAGE_ADAPTER,
         logic_adapters=constants.LOGIC_ADAPTERS,
-        database_uri=constants.DB_URL
+        database_uri=constants.DB_URL,
+        preprocessors= constants.PREPROCESSORS,
+        response_selection_method= constants.RESPONSE_SELECTION_METHOD
     )
     return bot
 
@@ -20,7 +23,7 @@ def init_chat_bot():
 def run_bot(bot):
     while True:
         try:
-            bot_input = bot.get_response(input())
+            bot_input = bot.get_response(raw_input())
             print(bot_input)
 
         except(KeyboardInterrupt, EOFError, SystemExit):
